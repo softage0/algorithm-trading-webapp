4a49ddea80fae36b1254acb70e02b33ef17dcf15
Wonju Jeon <softage0@gmail.com>
2016-08-08 10:06:32 +0900

    Stock detail and chart have been merged in stock list.

--- kiwoom/urls.py
+++ kiwoom/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^account_info/$', views.account_info, name='account_info'),
     url(r'^stock_list/$', views.stock_list, name='stock_list'),
     url(r'^stock_list/(?P<market_type>[0-9]+)$', views.stock_detail_list, name='stock_detail_list'),
-    url(r'^basic_info/(?P<code>[0-9]+)$', views.basic_info, name='basic_info'),
-    url(r'^chart/(?P<code>[0-9]+)$', views.chart, name='chart'),
+    url(r'^basic_info/(?P<code>\w+)$', views.basic_info, name='basic_info'),
+    url(r'^chart/(?P<code>\w+)$', views.chart, name='chart'),
     url(r'^api_docs/$', views.api_docs, name='api_docs'),
 ]
